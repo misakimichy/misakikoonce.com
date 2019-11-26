db942edb33f6a605bbc6009d2ba93c7629bba410
Misaki Koonce <misaki.koonce@gmail.com>
2019-11-26 10:27:31 -0800

    Add custom fields in markdown frontmatter.

--- src/markdown-pages/brown-and-cony.md
+++ src/markdown-pages/brown-and-cony.md
@@ -2,6 +2,7 @@
 path: "/works/brown-and-cony"
 date: "2019-05-04"
 title: "Brown and Cony"
+workUrl: "https://github.com/misakimichy/brown-and-cony"
 ---
 This page is about Brown and Cony project.
 Here is the test for iframe. Source URL is from Gatsby official tutorial.
--- src/templates/workTemplate.js
+++ src/templates/workTemplate.js
@@ -6,11 +6,13 @@ export default function Template({
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
+  
   return (
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
+        <a>{frontmatter.workUrl}</a>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
@@ -28,6 +30,7 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        workUrl
       }
     }
   }
