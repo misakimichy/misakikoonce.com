de5a7d9c231756e523d2090ac4f3707d2c8c9e29
Misaki Koonce <misaki.koonce@gmail.com>
2019-11-27 17:47:15 -0800

    Add resume to home sidebar.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,35 +2,34 @@ import React from "react"
 import { css } from "@emotion/core"
 import { Link } from "gatsby"
 import { rhythm } from "../utils/typography"
+import { Container, Sidebar } from "../styles/styles"
 
 export default ({ children }) => (
-  <div
-    css={css`
-      margin: 0 auto;
-      max-width: 700px;
-      padding: ${rhythm(2)};
-      padding-top: ${rhythm(1.5)};
-    `}
-  >
-    <Link to={`/`}>
-      <h1
+  <Container>
+    <Sidebar>
+      <Link to={`/`}>
+        <h1
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          Misaki Koonce
+        </h1>
+      </Link>
+      <p>
+        <a href="resume/ResumeMisakiKoonce.pdf">Resume</a>
+      </p>
+      <Link
+        to={`/about/`}
         css={css`
-          margin-bottom: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
+          float: right;
         `}
       >
-        Misaki Koonce
-      </h1>
-    </Link>
-    <Link
-      to={`/about/`}
-      css={css`
-        float: right;
-      `}
-    >
-      About
-    </Link>
+        About
+      </Link>
+    </Sidebar>
     {children}
-  </div>
+  </Container>
 )
---
+++ src/styles/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+    width: 100%
+    display: flex;
+`
+
+export const Sidebar = styled.div`
+    width: 20%;
+    height: 100vh
+    border-right: 1px solid #efefef
+`
