3aa2d7078e1b33b8b599d67359e040e3250001c5
Misaki Koonce <misaki.koonce@gmail.com>
2019-12-05 13:31:41 -0800

    Change name of directory from src/markdown-pages to src/markdown/works

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                path: `${__dirname}/src/markdown-pages`,
+                path: `${__dirname}/src/markdown-works`,
                 name: `works`,
             },
         },
