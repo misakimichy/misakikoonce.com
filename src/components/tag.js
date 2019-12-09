9e0b5c1f9ab5d62371b80845cc656ef58bd339e8
Misaki Koonce <misaki.koonce@gmail.com>
2019-12-09 13:08:10 -0800

    Add style to card tag.

--- src/components/tag.js
+++ src/components/tag.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { kebabCase } from 'lodash'
-import { Li } from '../styles/styles'
 
 const Tag = ({ name, count }) => (
-    <Li>
-        <Link to={`/tags/${kebabCase(name)}`}>{name} {count !== null ? (count) : ''}</Link>
-    </Li>
+    <li className="tag-item">
+        <Link
+            className="no-underline"
+            to={`/tags/${kebabCase(name)}`}
+        >
+            {name} {count !== null ? (count) : ''}
+        </Link>
+    </li>
 );
 
 export default Tag
