f0606f5c80f900e67592746bf4a5888d30e228ae
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-05 14:46:04 -0800

    Add responsive designs to Layout and Sidebar components.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faFile as faFileRegular } from '@fortawesome/free-regular-svg-icons'
 import { faCoffee, faFile } from '@fortawesome/free-solid-svg-icons'
 import { config } from '@fortawesome/fontawesome-svg-core'
+import './styles.css'
 
 library.add(fab, faCoffee, faFileRegular, faFile)
 config.autoAddCss = false;
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -39,39 +39,37 @@ const Sidebar = () => (
         `}
         render={data => (
             <>
-                <aside>
-                    <div>
-                        <Link to="/" className="no-underline">
-                            <img className="profile-image" src={data.site.siteMetadata.logo} />
-                        </Link>
-                        <h1>{data.site.siteMetadata.title}</h1>
-                        <p>{data.site.siteMetadata.description}</p>
-                        {/* <Link to="/about/">About</Link> */}
-                        <div className="social-links">
-                            <a className="no-underline social-link-item" href={data.site.siteMetadata.resume}>
-                                <div>
-                                    <FontAwesomeIcon icon={['far', 'file']} className="icon"/>
-                                    <span className="icon-span">Resume</span>
-                                </div>
-                            </a>
-                            <SocialIcon
-                                href={data.site.siteMetadata.socialLinks.twitter}
-                                icon={['fab', 'twitter']}
-                                name={'Twitter'}
-                            />
-                            <SocialIcon
-                                href={data.site.siteMetadata.socialLinks.github}
-                                icon={['fab', 'github-alt']}
-                                name={'GitHub'}
-                            />
-                            <SocialIcon
-                                href={data.site.siteMetadata.socialLinks.linkedin}
-                                icon={['fab', 'linkedin-in']}
-                                name={'LinkedIn'}
-                            />
-                        </div>
+                <div class='sidebar-container'>
+                    <Link to="/" className="no-underline">
+                        <img className="profile-image" src={data.site.siteMetadata.logo} />
+                    </Link>
+                    <h1>{data.site.siteMetadata.title}</h1>
+                    <p>{data.site.siteMetadata.description}</p>
+                    {/* <Link to="/about/">About</Link> */}
+                    <div className="social-links">
+                        <a className="no-underline social-link-item" href={data.site.siteMetadata.resume}>
+                            <div>
+                                <FontAwesomeIcon icon={['far', 'file']} className="icon"/>
+                                <span className="icon-span">Resume</span>
+                            </div>
+                        </a>
+                        <SocialIcon
+                            href={data.site.siteMetadata.socialLinks.twitter}
+                            icon={['fab', 'twitter']}
+                            name={'Twitter'}
+                        />
+                        <SocialIcon
+                            href={data.site.siteMetadata.socialLinks.github}
+                            icon={['fab', 'github-alt']}
+                            name={'GitHub'}
+                        />
+                        <SocialIcon
+                            href={data.site.siteMetadata.socialLinks.linkedin}
+                            icon={['fab', 'linkedin-in']}
+                            name={'LinkedIn'}
+                        />
                     </div>
-                </aside>
+                </div>
             </>
         )}
     />
