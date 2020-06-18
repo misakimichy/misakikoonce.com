cdb5d474de273b1c9ab4194ad3a473369a0e4771
Misaki Koonce <misaki.koonce@gmail.com>
2020-06-18 13:22:16 -0700

    Update profile image.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,76 +1,73 @@
 module.exports = {
-    siteMetadata: {
-        title: "Misaki Koonce",
-        logo: '/misaki-real.png',
-        author: "Misaki Koonce",
-        siteUrl: "https://misakikoonce.com",
-        description: "Software Engineer. Originally from Tokyo and now in Seattle. I enjoy frontend software development.",
-        socialLinks: {
-            twitter: "https://twitter.com/misakimichy",
-            github: "https://github.com/misakimichy",
-            linkedin: "https://www.linkedin.com/in/misakikoonce",
-        },
-        contact: {
-            email: "email@test.com"
-        },
-        resume: "/ResumeMisakiKoonce.pdf"
+  siteMetadata: {
+    title: "Misaki Koonce",
+    logo: '/misaki-real.jpeg',
+    author: "Misaki Koonce",
+    siteUrl: "https://misakikoonce.com",
+    description: "Software Engineer. Originally from Tokyo and now in Seattle. I enjoy frontend software development.",
+    socialLinks: {
+      twitter: "https://twitter.com/misakimichy",
+      github: "https://github.com/misakimichy",
+      linkedin: "https://www.linkedin.com/in/misakikoonce",
     },
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/src/markdown-projects`,
-                name: `projects`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/static`,
-                name: `profile`,
-            },
-        },
+    contact: {
+      email: "email@test.com"
+    },
+    resume: "/ResumeMisakiKoonce.pdf"
+  },
+  plugins: [{
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown-projects`,
+        name: `projects`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static`,
+        name: `profile`,
+      },
+    },
 
-        {
-            resolve: `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+            resolve: `gatsby-remark-images`,
             options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                ],
+              maxWidth: 590,
             },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-typography`,
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              pathToConfigModule: `src/utils/typography`,
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
-        },
-        `gatsby-plugin-feed`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-            },
-        },
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-              pathToConfigModule: `src/utils/typography`,
-            },
-        },
-        `gatsby-plugin-slug`,
-    ],
+          },
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {},
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-plugin-slug`,
+  ],
 }
