0440db3f8facbcf01eac183360cf161081013465
Misaki Koonce <misaki.koonce@gmail.com>
2020-06-18 13:35:38 -0700

    Update resume with new profile image.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     logo: '/misaki-real.jpeg',
     author: "Misaki Koonce",
     siteUrl: "https://misakikoonce.com",
-    description: "Software Engineer. Originally from Tokyo and now in Seattle. I enjoy frontend software development.",
+    description: "Software Engineer. From Tokyo Now in Seattle. I like developing application with React.",
     socialLinks: {
       twitter: "https://twitter.com/misakimichy",
       github: "https://github.com/misakimichy",
